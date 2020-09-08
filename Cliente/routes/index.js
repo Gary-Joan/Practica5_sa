6d3f10fcb9aa73450ee4951bafb9a253d03771aa
Gary Ortiz <goritz1490@gmail.com>
2020-09-08 10:40:21 -0600

    gitflow-feature-stash: agregar_practica4

---
+++ Cliente/routes/index.js
@@ -0,0 +1,63 @@
+var express = require("express");
+var router = express.Router();
+const axios = require("axios");
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+let menu = "";
+/* POST menu info */
+router.post("/submit", (req, res) => {
+
+  const pedido = { cliente: req.body.cliente, 
+                     menu: req.body.menu,
+                     direccion: req.body.direccion,
+                     origen:'Cliente',
+                     destino:'Restaurante',
+                    estado:'Enviado a Restaurante' };
+  llamar("http://localhost:3020/submit", JSON.stringify(pedido));//metodo para mandar los datos al ESB
+  menu = req.body.menu;
+  res.redirect("/");
+  
+  res.end();
+});
+/* GET Status of the Order. */
+//Con esta ruta verificamos siempre el estado pidiendo la informacion a ESB
+router.get("/Estado", async (req, res, next) => {
+  let res1 = await axios
+    .get("http://localhost:3020/Estado")
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  let data = res1.data;
+  res.render("RespuestaR", { respuesta: data });
+  res.end();
+});
+/* Funcion para poder hacer el POST a una url */
+async function llamar(ui, p) {
+  var resul = {
+    estado: "error",
+    mensaje: "no se a enviado",
+  };
+  try {
+    var re = await axios
+      .post(ui, p, { headers: { "content-type": "application/json" } })
+      .then(function (response) {
+        console.log("todo correcto");
+        console.log("mensaje " + p);
+        resul = response.data;
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log("error no encuentra el uri");
+      })
+      .then(function () {
+        console.log("siempre ejecutando");
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = router;
---
+++ ESB/app.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const path = require("path");
+const bodyparser = require("body-parser");
+const router = express.Router();
+const PORT = 3020;
+const { Console } = require("console");
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/index.html"));
+});
+
+//manejadores de pedido simples arrays
+let listaMensajes = new Array();
+//Esta ruta va ser la encargada de tomar el mensaje que vengo de los servidores y redirigirlos a donde pertenezcan
+app.post("/submit", function (request, response) {
+  //usando un metodo if para ver dentro del mensaje cual es el destino y cual es origen para saber hacia donde va y de donde viene
+  if (request.body.destino == "Restaurante") {
+    listaMensajes.push(request.body);
+    listaMensajes[0].estado = "Pedido Ingresado en Restaurante";
+    console.log(
+      "Pedido de cliente " + request.body.cliente + " ingresado a Restaurante."
+    );
+    llamar("http://localhost:3010/submit", listaMensajes.shift());
+  } else if (
+    request.body.origen == "Restaurante" &&
+    request.body.destino == "Cliente"
+  ) {
+    listaMensajes.push(request.body);
+    listaMensajes[0].estado = "Su Pedido se esta procesando en Restaurante";
+    console.log(
+      "Pedido de cliente " +
+        request.body.cliente +
+        " se esta procesando en el Restaurante."
+    );
+  } else if (
+    request.body.destino == "Repartidor" &&
+    request.body.origen == "Restaurante"
+  ) {
+    listaMensajes.push(request.body);
+    listaMensajes[0].estado = "Su pedido fue enviado al repatidor";
+    console.log(
+      "Pedido de cliente " + request.body.cliente + " enviado a Repartidor"
+    );
+    llamar("http://localhost:3005/submit", listaMensajes.shift());
+  }
+  else if (
+    request.body.destino == "Cliente" &&
+    request.body.origen == "Repartidor"
+  ) {
+    listaMensajes.push(request.body);
+    listaMensajes[0].estado = "Su pedido fue entregado";
+    console.log(
+      "Pedido de cliente " + request.body.cliente + " enviado con exito"
+    );
+  } else {
+    console.log("Error!");
+  }
+});
+//esta ruta devuelve el estado segund el mensaje actual que tengo en el sistema
+app.get("/Estado", function (req, res, next) {
+  if (listaMensajes.length > 0) {
+    console.log("Estado del pedido: " + listaMensajes[0].estado);
+    res.send("Estado del pedido: " + listaMensajes[0].estado);
+  } else {
+    console.log("No hay hecho ningun pedido a Restaurante");
+    res.send("No hay hecho ningun pedido a Restaurante!!");
+  }
+});
+
+/* Funcion para hacer los post segun la url mandada. */
+async function llamar(ui, p) {
+  var resul = {
+    estado: "error",
+    mensaje: "no se a enviado",
+  };
+  try {
+    var re = await axios
+      .post(ui, p, { headers: { "content-type": "application/json" } })
+      .then(function (response) {
+        console.log("todo correcto");
+        console.log("mensaje " + p);
+        resul = response.data;
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log("error no encuentra el uri");
+      })
+      .then(function () {
+        console.log("siempre ejecutando");
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+/* Escuchando en el puerte predefinido. */
+app.listen(PORT, () => {
+  console.log(`ESB escuchando en el puerto: ${PORT}`);
+});
